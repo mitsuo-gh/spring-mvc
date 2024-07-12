b645e2dda8db0089ca9dbbb69d6354d11b646415
mitsuo-gh <paruku.2225557@gmail.com>
2024-07-12 16:37:25 +0900

    vlidator学習

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,14 +1,30 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.demo.model.Inquiry;
 import com.example.demo.model.User;
 
 @Controller
 public class HomeController {
+	@GetMapping("/")
+	private String index(@ModelAttribute Inquiry inquiry) {
+		return "index";
+	}
+	
+	@PostMapping("/")
+	public String confirm(@Validated @ModelAttribute Inquiry inquiry, BindingResult result) {
+		if (result.hasErrors()) {
+			return "index";
+		}
+		return "confirm";
+	}
+	
 	@GetMapping("/form")
 	private String readForm(@ModelAttribute User user) {
 		return "form";
